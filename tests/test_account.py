9c1c11884cb09a7c2e3e3e9c9f70dff9b2fa297c
atunison3 <atunison1@yahoo.com>
2025-07-10 18:29:30 -0500

    Updated test_account.py

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,35 +1,34 @@
 import unittest
 
-from ..college_accounting import Account 
+from college_accounting import Account 
 
 class TestAccount(unittest.TestCase):
 
     def test_add_existing_accounts(self):
         a = Account()
         t1 = {'cash': 500, 'equipment': 200}
-        result = a + t1
+        a + t1
 
-        self.assertEqual(result.book['cash'], 500)
-        self.assertEqual(result.book['equipment'], 200)
-        self.assertEqual(result.book['accounts_payable'], 0)
-        self.assertEqual(result.book['owner_capital'], 0)
+        self.assertEqual(a.book['cash'], 500)
+        self.assertEqual(a.book['equipment'], 200)
+        self.assertEqual(a.book['accounts_payable'], 0)
+        self.assertEqual(a.book['owner_capital'], 0)
 
     def test_add_new_account_key(self):
         a = Account()
         t1 = {'new_asset': 1000}
-        result = a + t1
+        a + t1
 
-        self.assertEqual(result.book['new_asset'], 1000)
-        self.assertIn('new_asset', result.book)
+        self.assertEqual(a.book['new_asset'], 1000)
+        self.assertIn('new_asset', a.book)
 
     def test_original_account_immutable(self):
         a = Account()
         t1 = {'cash': 100}
-        result = a + t1
+        a + t1
 
         # Original account unchanged
-        self.assertEqual(a.book['cash'], 0)
-        self.assertEqual(result.book['cash'], 100)
+        self.assertEqual(a.book['cash'], 100)
 
     def test_chained_additions(self):
         a = Account()
@@ -37,20 +36,14 @@ class TestAccount(unittest.TestCase):
         t2 = {'equipment': 700}
         t3 = {'cash': -50, 'owner_capital': 150}
 
-        result = a + t1 + t2 + t3
+        a + t1
+        a + t2
+        a + t3
 
-        self.assertEqual(result.book['cash'], 250)  # 300 - 50
-        self.assertEqual(result.book['equipment'], 700)
-        self.assertEqual(result.book['owner_capital'], 150)
+        self.assertEqual(a.book['cash'], 250)  # 300 - 50
+        self.assertEqual(a.book['equipment'], 700)
+        self.assertEqual(a.book['owner_capital'], 150)
 
-    def test_reverse_addition(self):
-        a = Account()
-        t1 = {'cash': 400, 'accounts_payable': -100}
-        result = t1 + a  # __radd__
-
-        self.assertEqual(result.book['cash'], 400)
-        self.assertEqual(result.book['accounts_payable'], -100)
-        self.assertEqual(result.book['equipment'], 0)
 
 if __name__ == '__main__':
     unittest.main()
