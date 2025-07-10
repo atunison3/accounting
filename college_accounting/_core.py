1477bfc25e2f2b1d7c8890d9dcd90f7ee6a5326c
atunison3 <atunison1@yahoo.com>
2025-07-10 18:09:33 -0500

    Reformatted

--- college_accounting/_core.py
+++ college_accounting/_core.py
@@ -1,11 +1,6 @@
 class Account:
     def __init__(self):
-        self.book = {
-            'cash': 0, 
-            'equipment': 0, 
-            'accounts_payable': 0, 
-            'owner_capital': 0
-        }
+        self.book = {"cash": 0, "equipment": 0, "accounts_payable": 0, "owner_capital": 0}
 
     def __add__(self, transaction: dict):
         for k, v in transaction.items():
@@ -13,6 +8,3 @@ class Account:
                 self.book[k] += v
             else:
                 self.book[k] = v
-        
-
-    
