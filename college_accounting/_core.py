2412baf74b02e49a362f3d618a1cde081672818d
atunison3 <atunison1@yahoo.com>
2025-07-11 18:23:09 -0500

    Updated _core.py

--- college_accounting/_core.py
+++ college_accounting/_core.py
@@ -1,6 +1,5 @@
-__all__ = [
-    'BalanceSheet'
-]
+__all__ = ["BalanceSheet"]
+
 
 class BalanceSheet:
     def __init__(self):
@@ -10,70 +9,69 @@ class BalanceSheet:
     @property
     def assets(self):
         return sum(self._assets.values())
-    
+
     @property
     def liabilities(self):
         return sum(self._liabilities.values())
 
-    @property 
+    @property
     def equity(self):
         return self.assets - self.liabilities
 
     def add_asset(self, new_asset: str, value: float):
-        '''Add asset to list'''
-        self._assets[new_asset[:12]] = value 
+        """Add asset to list"""
+        self._assets[new_asset[:12]] = value
 
     def add_liability(self, new_liability: str, value: float):
-        '''Add new liability'''
-        self._liabilities[new_liability[:12]] = value 
+        """Add new liability"""
+        self._liabilities[new_liability[:12]] = value
 
     def print_balance_sheet(self):
-        '''Prints the balance sheet in a clean format'''
+        """Prints the balance sheet in a clean format"""
 
         width = 36
-        
-        print('=' * width)
-        n = (width - 14) // 2
-        print(' ' * n + 'BALANCE SHEET' + ' ' * n)
-        print('=' * width)
 
-        print('Assets')
+        print("=" * width)
+        n = (width - 14) // 2
+        print(" " * n + "BALANCE SHEET" + " " * n)
+        print("=" * width)
+
+        print("Assets")
         for i, (k, v) in enumerate(self._assets.items()):
             if i == 0:
-                line = f'    {k}'
+                line = f"    {k}"
                 n = width - 5 - len(k) - len(str(v))
-                line += ' ' * n
-                line += f'${v:d}'
+                line += " " * n
+                line += f"${v:d}"
             else:
-                line = f'    {k}'
+                line = f"    {k}"
                 n = width - 4 - len(k) - len(str(v))
-                line += ' ' * n
-                line += f'{v:d}'                
+                line += " " * n
+                line += f"{v:d}"
             print(line)
-        print('Total Assets')
-        print('=' * width)
+        print("Total Assets")
+        print("=" * width)
 
-        print('Liabilities')
+        print("Liabilities")
         # Fill in liabilities
         for i, (k, v) in enumerate(self._liabilities.items()):
             if i == 0:
-                line = f'    {k}'
+                line = f"    {k}"
                 n = width - 5 - len(k) - len(str(v))
-                line += ' ' * n
-                line += f'${v:d}'
+                line += " " * n
+                line += f"${v:d}"
             else:
-                line = f'    {k}'
+                line = f"    {k}"
                 n = width - 4 - len(k) - len(str(v))
-                line += ' ' * n
-                line += f'{v:d}'                
+                line += " " * n
+                line += f"{v:d}"
             print(line)
         liabilities = self.liabilities
         n = width - 18 - len(str(liabilities))
-        line = f'Total Liabilities' + ' ' * n + f'${liabilities}'
+        line = f"Total Liabilities" + " " * n + f"${liabilities}"
         print(line)
-        print('=' * width)
+        print("=" * width)
 
         equity = self.equity
-        line = 'Total Equity' + ' ' * (width - 13 - len(str(equity))) + f'${equity}'
+        line = "Total Equity" + " " * (width - 13 - len(str(equity))) + f"${equity}"
         print(line)
-
