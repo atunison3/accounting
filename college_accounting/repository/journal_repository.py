bec5e1804364eb8f63b00c4070f56938c69f2cff
atunison3 <atunison1@yahoo.com>
2025-07-14 21:00:27 -0500

    Added repository

---
+++ college_accounting/repository/journal_repository.py
@@ -0,0 +1,23 @@
+from ..domain.journal import Journal 
+
+class JournalRepository:
+
+    @abstractmethod
+    def add(self, transaction: Transaction) -> None:
+        """Add a transaction to the repository."""
+        pass
+
+    @abstractmethod
+    def get_by_id(self, transaction_id: int) -> Transaction:
+        """Retrieve a transaction by its ID."""
+        pass
+
+    @abstractmethod
+    def list_all(self) -> List[Transaction]:
+        """Return all transactions."""
+        pass
+
+    @abstractmethod
+    def delete(self, transaction_id: int) -> None:
+        """Delete a transaction by its ID."""
+        pass
